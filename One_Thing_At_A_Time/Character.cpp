ef562b8cd12e42cd2d098a0ec57b73a0ffd435ad
marcus.bean <marcus.bean@gd-ms.com>
2018-12-24 16:18:31 -0700

    Create enums stat, status, target; structs PrimaryStats DerivedStats, SpecialAbility; and class Skill; added to Character. Redefined printStatus function.

--- One_Thing_At_A_Time/Character.cpp
+++ One_Thing_At_A_Time/Character.cpp
@@ -24,3 +24,124 @@ void PC::printInventory()
 	}
 	std::cout << "." << std::endl;
 }
+
+inline void Character::addMaxHp(int inputMaxHp)
+{
+	derivedStatsBonus.maxHp += inputMaxHp;
+	calculateDerivedStats();
+}
+
+inline void Character::addCurrentHp(int inputCurrentHp)
+{
+	derivedStatsBonus.currentHp += inputCurrentHp;
+	calculateDerivedStats();
+}
+
+inline void Character::addSpeed(int inputSpeed)
+{
+	derivedStatsBonus.speed += inputSpeed;
+	calculateDerivedStats();
+}
+
+inline void Character::addMeleeAcc(int inputMeleeAcc)
+{
+	derivedStatsBonus.meleeAcc += inputMeleeAcc;
+	calculateDerivedStats();
+}
+
+inline void Character::addMeleeDamage(int inputMeleeDmg)
+{
+	derivedStatsBonus.meleeDmg += inputMeleeDmg;
+	calculateDerivedStats();
+}
+
+inline void Character::addRangedAcc(int inputRangedAcc)
+{
+	derivedStatsBonus.rangedAcc += inputRangedAcc;
+	calculateDerivedStats();
+}
+
+inline void Character::addRangedDmg(int inputRangedDmg)
+{
+	derivedStatsBonus.rangedDmg += inputRangedDmg;
+	calculateDerivedStats();
+}
+
+void Character::calculateDerivedStats()
+{ /* TODO: Computation */
+	derivedStats.maxHp = /*Computation +*/ derivedStatsBonus.maxHp;
+	derivedStats.currentHp = /*Computation +*/ derivedStatsBonus.currentHp;
+	derivedStats.meleeAcc = /*Computation +*/ derivedStatsBonus.meleeAcc;
+	derivedStats.meleeDmg = /*Computation +*/ derivedStatsBonus.meleeDmg;
+	derivedStats.rangedAcc = /*Computation +*/ derivedStatsBonus.rangedAcc;
+	derivedStats.meleeDmg = /*Computation +*/ derivedStatsBonus.meleeDmg;
+	derivedStats.speed = /*Computation +*/ derivedStatsBonus.speed;
+}
+
+void Character::printStatus()
+{
+	std::cout << "You are ";
+	switch (getStatus())
+	{
+		case STATUS_NONE:
+		{
+			std::cout << "fine.\n";
+			break;
+		}
+		case STATUS_DIRTY:
+		{
+			std::cout << "dirty.\n";
+			break;
+		}
+		case STATUS_DEAD:
+		{
+			std::cout << "dead.\n";
+			break;
+		}
+		case STATUS_UNCONSCIOUS:
+		{
+			std::cout << "unconscious.\n";
+			break;
+		}
+		case STATUS_HIDDEN:
+		{
+			std::cout << "hidden.\n";
+			break;
+		}
+		case STATUS_CONFUSED:
+		{
+			std::cout << "confused.\n";
+			break;
+		}
+		case STATUS_BURNED:
+		{
+			std::cout << "burned.\n";
+			break;
+		}
+		case STATUS_POISONED:
+		{
+			std::cout << "poisoned.\n";
+			break;
+		}
+		case STATUS_FROZEN:
+		{
+			std::cout << "frozen.\n";
+			break;
+		}
+		case STATUS_RECOILING:
+		{
+			std::cout << "recoiling.\n";
+			break;
+		}
+		case STATUS_BLEEDING:
+		{
+			std::cout << "bleeding.\n";
+			break;
+		}
+		case STATUS_HUNGRY:
+		{
+			std::cout << "hungry.\n";
+			break;
+		}
+	}
+}
--- One_Thing_At_A_Time/Character.h
+++ One_Thing_At_A_Time/Character.h
@@ -5,39 +5,190 @@
 #include <vector>
 #include "object.h"
 
+enum Status_Type
+{
+	STATUS_NONE,
+	STATUS_DIRTY,
+	STATUS_DEAD,
+	STATUS_UNCONSCIOUS,
+	STATUS_HIDDEN,
+	STATUS_CONFUSED,
+	STATUS_BURNED,
+	STATUS_POISONED,
+	STATUS_FROZEN,
+	STATUS_RECOILING,
+	STATUS_BLEEDING,
+	STATUS_HUNGRY
+};
+
+enum Target_Type
+{
+	TARGET_NONE,
+	TARGET_SELF,
+	TARGET_SINGLE_CHARACTER,
+	TARGET_GROUP,
+	TARGET_AREA_EFFECT,
+	TARGET_LOCATION,
+	TARGET_FEATURE,
+	TARGET_ITEM,
+	MAX_TARGET
+};
+
+struct PrimaryStats
+{
+	/* Primary Stats */
+	/* Strength */
+	int st;
+	/* Dexterity */
+	int dx;
+	/* Intelligence */
+	int iq;
+	/* Health */
+	int ht;
+};
+
+struct DerivedStats
+{
+	/* Maximum Hit Points */
+	int maxHp;
+	/* Current Hit Points */
+	int currentHp;
+	/* Speed */
+	int speed;
+	/* Melee Accuracy */
+	int meleeAcc;
+	/* Melee Damge */
+	int meleeDmg;
+	/* Ranged Accuracy */
+	int rangedAcc;
+	/* Ranged Damage */
+	int rangedDmg;
+};
+
+enum Character_Stat
+{
+	CHARACTER_STAT_NONE,	/* None					*/
+	ST,						/* Strenth				*/
+	DX,						/* Dexterity			*/
+	IQ,						/* Intelligence			*/
+	HT,						/* Health				*/
+	MAX_HP,					/* Maximum Hit Points	*/
+	SPEED,					/* Speed				*/
+	MELEE_ACC,				/* Melee Accuracy		*/
+	MELEE_DMG,				/* Melee Damage			*/
+	RANGED_ACC,				/* Ranged Accuracy		*/
+	RANGED_DMG,				/* Ranged Damage		*/
+	IS_STUCK,				/* Is Stuck				*/
+	IS_DEAD,				/* Is Dead				*/
+	MAX_CHARACTER_STAT		/* Maximum number		*/
+};
+
+struct SpecialAbility
+{
+	std::string name;
+	Target_Type target;
+	std::vector<Character_Stat> statsAffected;
+	int duration;
+	int usesPerDay;
+};
+
+class Skill
+{
+public:
+	std::string name;
+	int skillLevel;
+	Target_Type target;
+	std::vector<Character_Stat> statsAffected;
+	unsigned int turnDuration;
+	int damage;
+	unsigned short usesPerDay;
+	virtual void useSkill() {};
+};
+
 class Character
 {
 private:
 	std::string name;
-	std::string status;
-	std::vector<Object*> inventory;
+	Status_Type status;
+	std::vector<Object *> inventory;
+	std::vector<Skill *> skillList;
+	std::vector<SpecialAbility *> specialAbilityList;
+	int gold;
+
+	/* Experience points */
+	int xp;
+	/* Level */
+	int lvl;
+
+	/* Primary Stats */
+	PrimaryStats primaryStats;
+
+	/* Derived Stats */
+	DerivedStats derivedStats;
+
+	/* Derived Stat Bonuses */
+	DerivedStats derivedStatsBonus;
+
+	std::vector<SpecialAbility> specialAbilities;
+	void calculateDerivedStats();
+	
 public:
 	/* Constructors */
-	Character() {};
-	Character(std::string inputName, std::string inputStatus) : name(inputName), status(inputStatus) {};
+	Character() {}
+	Character(std::string inputName, Status_Type inputStatus) : name(inputName), status(inputStatus) {}
 
 	/* Accessors */
-	const std::string & getStatus() { return status; };
-	std::vector<Object*> & getInventory() { return inventory; };
-	std::string getName() { return name; };
+	std::string getName() { return name; }
+	const Status_Type getStatus() { return status; }
+	std::vector<Object*> & getInventory() { return inventory; }
+	int getGold() { return gold; }
+	int getXp() { return xp; }
+	int getLvl() { return lvl; }
+	int getSt() { return primaryStats.st; }
+	int getDx() { return primaryStats.dx; }
+	int getIq() { return primaryStats.iq; }
+	int getHt() { return primaryStats.ht; }
+	int getMaxHp() { return derivedStats.maxHp; }
+	int getCurrentHp() { return derivedStats.currentHp; }
+	int getMeleeAcc() { return derivedStats.meleeAcc; }
+	int getMeleeDmg() { return derivedStats.meleeDmg; }
+	int getRangedAcc() { return derivedStats.rangedAcc; }
+	int getRangedDmg() { return derivedStats.rangedDmg; }
+	int getSpeed() { return derivedStats.speed; }
+	std::vector<Skill *> getSkillList() { return skillList; }
+	std::vector<SpecialAbility *> getSpecialAbilityList() { return specialAbilityList; }
+
 
 	/* Mutators */
-	void setStatus(std::string lhs) { status = lhs; };
-	void setInventory(std::vector<Object*> inputInventory) { inventory = inputInventory; };
-	void pushInventory(Object *inputObject) { inventory.push_back(inputObject); };
-	void setName(std::string inputName) { name = inputName; };
+	void setName(std::string inputName) { name = inputName; }
+	void setStatus(Status_Type inputStatus) { status = inputStatus; }
+	void setInventory(std::vector<Object*> inputInventory) { inventory = inputInventory; }
+	void pushInventory(Object *inputObject) { inventory.push_back(inputObject); }
+	void addGold(int inputGold) { gold += inputGold; }
+	void addXp(int inputXp) { xp += inputXp; }
+	void levelUp(); // TODO: Write levelUp()
+	void setSt(int inputSt) { primaryStats.st = inputSt; }
+	void setDx(int inputDx) { primaryStats.dx = inputDx; }
+	void setIq(int inputIq) { primaryStats.iq = inputIq; }
+	void setHt(int inputHt) { primaryStats.ht = inputHt; }
+	void addMaxHp(int inputMaxHp);
+	void addCurrentHp(int inputCurrentHp);
+	void addSpeed(int inputSpeed); 
+	void addMeleeAcc(int inputMeleeAcc);
+	void addMeleeDamage(int inputMeleeDmg);
+	void addRangedAcc(int inputRangedAcc);
+	void addRangedDmg(int inputRangedDmg);
 
 	// Virtual Utilities
-	virtual void printInventory() {};
-	virtual void printStatus() {};
+	virtual void printInventory() {}
+	virtual void printStatus();
 };
 
 class PC : public Character
 {
 public:
-	PC() { setName("pc"), setStatus("dirty"); }
+	PC() { setName("pc"), setStatus(STATUS_DIRTY); }
 
 	// Virtual Utilities
-	void printStatus() { std::cout << "You are " << getStatus() << "." << std::endl; }
 	void printInventory();
 };
